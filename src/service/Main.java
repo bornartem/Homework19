07fe6e1b29580e6c46e55094655bef96d70a643d
bornartem <born.artem@gmail.com>
2023-10-04 21:42:34 +0300

    2 commit

---
+++ src/service/Main.java
@@ -0,0 +1,10 @@
+package service;
+
+import service.ServiceParameter;
+
+public class Main {
+public static ServiceParameter serviceParameter;
+    public static void main (String[] args) {
+        serviceParameter.getParameter();
+    }
+}
